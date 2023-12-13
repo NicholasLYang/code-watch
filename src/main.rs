c5114865ac39e7092051e1ffa1a2f2fa2aa5308b
Nicholas Yang <nick@nicholasyang.com>
2023-12-12 22:52:38 -0500

    Wrote watcher that makes commits every few seconds

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,107 @@
-use git2::Index;
+use git2::{Index, Oid, Repository};
+use std::path::Path;
+use std::time::Duration;
+use tokio::time::interval;
 
-fn main() -> Result<(), anyhow::Error> {
-    create_tree()?;
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let watcher = Watcher::new(".")?;
+    let mut interval = interval(Duration::from_secs(5));
 
-    Ok(())
+    loop {
+        interval.tick().await;
+        watcher.watch()?;
+    }
 }
 
-fn create_tree() -> Result<(), anyhow::Error> {
-    let mut index = Index::new()?;
-    index.add_all(["*"].iter(), git2::IndexAddOption::DEFAULT, None)?;
-    let oid = index.write_tree()?;
-    println!("{:?}", oid);
+const CODE_WATCH_HEAD: &str = "CODE_WATCH_HEAD";
+struct Watcher {
+    repo: Repository,
+}
 
-    Ok(())
+impl Watcher {
+    fn new(path: impl AsRef<Path>) -> Result<Self, anyhow::Error> {
+        let repo = Repository::open(path.as_ref())?;
+
+        Ok(Self { repo })
+    }
+
+    fn watch(&self) -> Result<(), anyhow::Error> {
+        // Check if up to date and if not, we create a new one
+        let code_watch_head = match self.get_code_watch_head() {
+            Some(code_watch_head)
+                if self.check_if_code_watch_head_is_up_to_date(code_watch_head)? =>
+            {
+                code_watch_head
+            }
+            _ => self.create_code_watch_head()?,
+        };
+
+        let tree = self.create_tree()?;
+        self.commit_tree(tree, code_watch_head)?;
+
+        Ok(())
+    }
+
+    // Commits tree
+    fn commit_tree(&self, tree: Oid, parent: Oid) -> Result<Oid, anyhow::Error> {
+        let tree = self.repo.find_tree(tree)?;
+        let parent = self.repo.find_commit(parent)?;
+        let signature = self.repo.signature()?;
+        let message = "Code watch commit";
+        let commit = self.repo.commit(
+            Some("CODE_WATCH_HEAD"),
+            &signature,
+            &signature,
+            message,
+            &tree,
+            &[&parent],
+        )?;
+
+        Ok(commit)
+    }
+
+    // Creates tree from temporary index of current repo state
+    fn create_tree(&self) -> Result<Oid, anyhow::Error> {
+        let index_file = Path::new(".git/code-watch-index");
+        let mut index = Index::open(index_file)?;
+        self.repo.set_index(&mut index)?;
+        index.add_all(["*"].iter(), git2::IndexAddOption::DEFAULT, None)?;
+
+        let oid = index.write_tree()?;
+        // Clear up the index for next time
+        index.clear()?;
+        index.write()?;
+
+        Ok(oid)
+    }
+
+    // Checks if CODE_WATCH_HEAD and HEAD have HEAD as a merge base. If not, then we need
+    // to update CODE_WATCH_HEAD to be off of HEAD
+    fn check_if_code_watch_head_is_up_to_date(
+        &self,
+        code_watch_head: Oid,
+    ) -> Result<bool, anyhow::Error> {
+        let head = self.repo.head()?.target().unwrap();
+
+        let merge_base = self.repo.merge_base(code_watch_head, head)?;
+
+        Ok(merge_base == head)
+    }
+
+    // Creates the `CODE_WATCH_HEAD` ref off of HEAD
+    fn create_code_watch_head(&self) -> Result<Oid, anyhow::Error> {
+        let head_id = self.repo.head()?.target().unwrap();
+        let code_watch_head =
+            self.repo
+                .reference(CODE_WATCH_HEAD, head_id, true, "Code watch head")?;
+
+        Ok(code_watch_head.target().unwrap())
+    }
+
+    fn get_code_watch_head(&self) -> Option<Oid> {
+        let head = self.repo.find_reference(&CODE_WATCH_HEAD).ok()?;
+
+        head.target()
+    }
 }
